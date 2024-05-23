ff5f0d9e6225acd85cc70abf31d907467306a669
Leeseunghoon <dl11911@naver.com>
2024-05-23 18:13:46 +0900

    test : article test page

---
+++ src/pages/article/ArticleTest.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+
+const ArticleTest = () => {
+    const [userDTO, setUserDTO] = useState({
+        id : 1,
+        articleTags:'자유게시판',
+        title : 'test1111',
+        content : 'test22222'
+
+    });
+
+    const onSubmit = (e)=>{
+        e.preventDefault()
+
+        axios.post('http://localhost:8080/articleWriteTest',userDTO)
+        .then(res=>
+            console.log(res.data)
+        )
+        .catch(error => 
+            console.log(error)
+        )
+
+    }
+    
+
+    return (
+        <div>
+
+            유저ID <input type='text' name='memberId' value={userDTO.id} />   
+            <br/>
+            게시판 종류 <input type='text' name='article_tags' value={userDTO.articleTags}/>
+            <br/>
+            제목 <input type='text' name='title' value={userDTO.title}/> 
+            <br/>
+            내용 <input type='text' name='content' value={userDTO.content}/>
+            <br/>
+            <Button onClick={ onSubmit }>전송 테스트</Button>
+        </div>
+    );
+};
+
+export default ArticleTest;
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -5,6 +5,7 @@ import testRouter from "./testRouter.jsx";
 const Loading = () => <>Loading...</>;
 const MainPage = lazy(() => import('./../pages/MainPage'));
 const TestPage = lazy(() => import('../pages/test/TestPage.jsx'));
+const ArticlePage = lazy(() => import('../pages/article/ArticleTest.jsx'));
 
 const root = createBrowserRouter([
     {
@@ -17,6 +18,11 @@ const root = createBrowserRouter([
         element: <Suspense fallback={<Loading />}><TestPage/></Suspense>, // 여기엔 그 기능의 인덱스나 메인 페이지 넣는듯
         children:testRouter()
     },
+    {
+        path:"article",
+        element: <Suspense fallback={<Loading />}><ArticlePage/></Suspense>,
+
+    }
 ]);
 
 export default root;
