97e95994f2044865856fe69dfaeabb4d94cd10bd
jyc961020 <jyc961020@naver.com>
2024-06-13 17:45:02 +0900

    hotspot update

--- src/pages/hotSpot/HotSpotPage.jsx
+++ src/pages/hotSpot/HotSpotPage.jsx
@@ -1,14 +1,11 @@
 import DefaultLayout from "../../layouts/DefaultLayout.jsx";
 import {Outlet} from "react-router-dom";
-import {HotSpotProvider} from "../../components/hotSpot/HotSpotContext.jsx";
 
 const HotSpotPage = () => {
 
     return (
         <DefaultLayout>
-            <HotSpotProvider>
             <Outlet/>
-            </HotSpotProvider>
         </DefaultLayout>
     );
 }
