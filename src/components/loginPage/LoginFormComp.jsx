75443457515203f44f0a77ae5d08d94b8269bfbe
DoDoBird <88835687+gyuchanlee@users.noreply.github.com>
2024-06-20 19:41:20 +0900

    fix: 로그인 페이지
    - 로그인 페이지 수정

--- src/components/loginPage/LoginFormComp.jsx
+++ src/components/loginPage/LoginFormComp.jsx
@@ -31,19 +31,13 @@ const LoginFormComp = () => {
     });
 
     useEffect(() => {
-        // URL에 error=true가 있는지 확인
         const queryParams = new URLSearchParams(location.search);
         if (queryParams.get('error') === 'true') {
             Toast.fire({
                 icon: 'error',
                 title: '로그인 실패',
-                width: 450
+                width: 300
             });
-            // Swal.fire({
-            //     title: '로그인 실패',
-            //     text: '다른 로그인 방법을 시도해 주세요.^^',
-            //     icon: 'warning'
-            // });
         }
     }, [location]);
 
@@ -55,26 +49,15 @@ const LoginFormComp = () => {
                 Toast.fire({
                     icon: 'error',
                     title: '로그인 실패',
-                    width: 450
+                    width: 300
                 })
-                // Swal.fire({
-                //     title: '로그인 실패',
-                //     text: '다른 로그인 방법을 시도해 주세요.^^',
-                //     icon: 'warning'
-                // });
-                // window.location.reload();
             } else {
                 console.log('로그인 성공');
                 Toast.fire({
                     icon: 'success',
                     title: '로그인 하였습니다',
-                    width: 450
+                    width: 300
                 });
-                // Swal.fire({
-                //     title: '로그인 성공',
-                //     text: '환영합니다.^^',
-                //     icon: 'success'
-                // });
                 dispatch(saveToken(token));
                 navigate({ pathname: '/' }, { replace: true });
             }
@@ -88,43 +71,45 @@ const LoginFormComp = () => {
     };
 
     return (
-        <div>
-            <div className={LoginStyle.body}>
-                <form className={LoginStyle.form}>
-                    <div className={LoginStyle.formGroup}>
-                        <label htmlFor="email" className={LoginStyle.label}>이메일</label>
-                        <input type="email"
-                            name="email"
-                            className={LoginStyle.input}
-                            placeholder="이메일 입력"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required />
-                    </div>
-
-                    <div className={LoginStyle.formGroup}>
-                        <label htmlFor="password" className={LoginStyle.label}>비밀번호</label>
-                        <input type="password"
-                            name="pwd"
-                            className={LoginStyle.input}
-                            placeholder="비밀번호 입력"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
-                    </div>
-
-                    <button className={LoginStyle.submitButton}
-                        onClick={requestLogin}
-                    >로그인</button>
-                </form>
-
-                <div className={LoginStyle.links}>
-                    <Link to="forgot-password" className={LoginStyle.link}>비밀번호를 잊으셨나요?</Link>
-                    <Link to="/join" className={LoginStyle.link}>회원가입</Link>
-                    <Link to={`${url}/oauth2/authorization/naver`} className={LoginStyle.link}>
-                        <img src='https://clova-phinf.pstatic.net/MjAxODAzMjlfOTIg/MDAxNTIyMjg3MzM3OTAy.WkiZikYhauL1hnpLWmCUBJvKjr6xnkmzP99rZPFXVwgg.mNH66A47eL0Mf8G34mPlwBFKP0nZBf2ZJn5D4Rvs8Vwg.PNG/image.png'className={LoginStyle.naverImg}/>
-                    </Link>
+        <div className={LoginStyle.body}>
+            <form className={LoginStyle.form}>
+                <div className={LoginStyle.formGroup}>
+                    <label htmlFor="email" className={LoginStyle.label}>이메일</label>
+                    <input type="email"
+                           name="email"
+                           className={LoginStyle.input}
+                           placeholder="이메일 입력"
+                           value={username}
+                           onChange={(e) => setUsername(e.target.value)}
+                           required />
                 </div>
+
+                <div className={LoginStyle.formGroup}>
+                    <label htmlFor="password" className={LoginStyle.label}>비밀번호</label>
+                    <input type="password"
+                           name="pwd"
+                           className={LoginStyle.input}
+                           placeholder="비밀번호 입력"
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}
+                           required />
+                </div>
+
+                <button className={LoginStyle.submitButton}
+                        onClick={requestLogin}
+                >로그인</button>
+            </form>
+
+            <div className={LoginStyle.links}>
+                <div className={LoginStyle.linksInline}>
+                    <Link to="forgot-password" className={LoginStyle.link}>비밀번호 찾기</Link>
+                    <span className={LoginStyle.separator}>|</span>
+                    <Link to="/join" className={LoginStyle.link}>회원가입</Link>
+                </div>
+                <Link to={`${url}/oauth2/authorization/naver`} className={LoginStyle.link}>
+                    <img
+                        src='https://clova-phinf.pstatic.net/MjAxODAzMjlfOTIg/MDAxNTIyMjg3MzM3OTAy.WkiZikYhauL1hnpLWmCUBJvKjr6xnkmzP99rZPFXVwgg.mNH66A47eL0Mf8G34mPlwBFKP0nZBf2ZJn5D4Rvs8Vwg.PNG/image.png'className={LoginStyle.naverImg}/>
+                </Link>
             </div>
         </div>
     );
